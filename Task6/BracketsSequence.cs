c8b823a37b8c985ae20508a20972b4d3f53ad743
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-13 13:37:08 +0300

    Update BracketsSequence.cs

--- Task6/BracketsSequence.cs
+++ Task6/BracketsSequence.cs
@@ -1,1 +1,74 @@
 
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+   public class Stack<T>
+   {
+      private List<T> elements;
+      private int size;
+      
+      
+      public Stack()
+      {
+       elements = new List<T>();
+       size = 0;
+      } 
+
+      public int Size() 
+      {
+       return size;
+      }
+
+      public T Pop()
+      {
+       if (size != 0)
+       {
+           T result = elements[size - 1];
+           elements.RemoveAt(size - 1);
+           --size;
+           return result;
+       }
+       return default(T); // null, если стек пустой
+      }
+	  
+      public void Push(T val)
+      {
+       elements.Add(val);
+       ++size;
+      }
+
+      public T Peek()
+      {
+       if (size != 0) return elements[size - 1];
+       return default(T); // null, если стек пустой
+      }
+   }
+
+
+    public class Program
+    {
+      public static bool IsRightBrackets(string str)
+      {
+          leftBrackets Stack<char>  = new Stack<char>();
+          foreach (char i in str)
+          {
+            if (i == '(') leftBrackets.Push(i);
+            else if (i == ')') 
+            {
+              if (leftBrackets.Pop() == defalt(char)) return false;
+            }
+          }
+          if (leftBrakets.size() != 0) return false;
+          return true;
+      }
+      public static void Main()
+      {
+          Console.WriteLine(IIsRightBrackets("(((())))"));
+      }
+    }
+    
+    
+}

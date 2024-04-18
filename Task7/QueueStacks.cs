ac92fd554b29138e066e8cac7656c29f3220dbeb
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-18 14:39:26 +0300

    Create QueueStacks.cs

---
+++ Task7/QueueStacks.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+   public class Queue<T>
+   { 
+      private Stack<T> elementsFirst;
+      private Stack<T> elementsSecond;
+      
+      public Queue()
+      {
+       elementsFirst = new Stack<T>();
+       elementsSecond = new Stack<T>();
+      } 
+
+      public void Enqueue(T item)
+      {
+        elementsFirst.Push(item);
+      }
+
+      public T Dequeue()
+      {
+        for (int i = elementsFirst.Count; i > 0; --i) elementsSecond.Push(elementsFirst.Pop());
+        if (elementsSecond.Count != 0) return elements.Second.Pop();
+        return default(T); // если очередь пустая
+      }
+
+      public int Size()
+      {
+        return elementsFirst.Count + elementsSecond.Count; // размер очереди
+      }
+
+   }
+}

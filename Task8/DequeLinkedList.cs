48a315c3b7d329cb4e4321254ce7856248db781c
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-21 19:28:09 +0300

    Create DequeLinkedList.cs

---
+++ Task8/DequeLinkedList.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+  class Deque<T>
+  {
+    private LinkedList<T> elements;
+    
+    public Deque()
+    {
+     LinkedList<T> elements = new LinkedList<T>();
+    }
+
+    public void AddFront(T item)
+    {
+     elements.AddFirst(item);
+    }
+
+    public void AddTail(T item)
+    {
+     elements.AddLast(T);
+    }
+
+    public T RemoveFront()
+    {
+     if (Size() != 0) 
+     {
+         T result = elements.First.Value;
+         elements.RemoveFirst();
+         return result;
+     }
+     return default(T);
+    }
+
+    public T RemoveTail()
+    {
+     if (Size() != 0) 
+     {
+         T result = elements.First.Value;
+         elements.RemoveLast();
+         return result;
+     }
+     return default(T);
+    }
+        
+    public int Size()
+    {
+     return elements.Count; // размер очереди
+    }
+  }
+
+}

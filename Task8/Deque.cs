9f9aa8e15f5c5eb0373e5ae2fb582b3f35ec0f63
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-21 17:22:47 +0300

    Create Deque.cs

---
+++ Task8/Deque.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+  class Deque<T>
+  {
+    private List<T> elements;
+    
+    public Deque()
+    {
+     List<T> elements = new List<T>();
+    }
+
+    public void AddFront(T item)
+    {
+     elements.Insert(0, item);
+    }
+
+    public void AddTail(T item)
+    {
+     elements.Add(item);
+    }
+
+    public T RemoveFront()
+    {
+     if (Size() != 0) 
+     {
+         T result = elements[0];
+         elements.RemoveAt(0);
+         return result;
+     }
+     return default(T);
+    }
+
+    public T RemoveTail()
+    {
+     if (Size() != 0) 
+     {
+         T result = elements[Size() - 1];
+         elements.RemoveAt(Size() - 1);
+         return result;
+     }
+     return default(T);
+    }
+        
+    public int Size()
+    {
+     return elements.Count; // размер очереди
+    }
+  }
+
+}
+

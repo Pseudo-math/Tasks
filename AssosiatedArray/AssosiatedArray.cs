65404dac30b5bb6313cc124496aa5db0ac4e9efd
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-05-06 23:24:14 +0300

    Update AssosiatedArray.cs

--- AssosiatedArray/AssosiatedArray.cs
+++ AssosiatedArray/AssosiatedArray.cs
@@ -6,39 +6,40 @@ namespace AlgorithmsDataStructures
 
   public class NativeDictionary<T>
   {
-    public int size;
+    public uint size;
     private const uint step = 2147483629; // Max prime Int32 number
     public string [] slots;
     public T [] values;
 
     public NativeDictionary(int sz)
     {
-      size = sz;
+      size = Convert.ToUInt32(sz);
       slots = new string[size];
       values = new T[size];
     }
 
     public int HashFun(string key)
     {
-      byte[] bytes = System.Text.Encoding.UTF8.GetBytes(value);
+      byte[] bytes = System.Text.Encoding.UTF8.GetBytes(key);
       int result = 0;
       foreach (var i in bytes)
         result += Convert.ToInt32(i);
-      return result % size;
+      return result % Convert.ToInt32(size);
     }
     
     public int SeekSlot(string value)
     {
-      uint slot = HashFun(value);
-      if (slots[slot] == null) return slot;
+      uint slot = Convert.ToUInt32(HashFun(value));
+      if (slots[slot] == null) return Convert.ToInt32(slot);
       for (uint i = (slot + step) % size; i != slot; i = (i + step) % size) // Unsigned????
-        if (slots[i] == null) return i;
+        if (slots[i] == null) return Convert.ToInt32(i);
+      return -1;
     }
     
     public bool IsKey(string key)
     {
-      uint slot = HashFun(key);
-      if (slots[slot] == key) return slot;
+      uint slot = Convert.ToUInt32(HashFun(key));
+      if (slots[slot] == key) return true;
       for (uint i = (slot + step) % size; i != slot; i = (i + step) % size)
         if (slots[i] == key) return true;
       return false;
@@ -53,10 +54,10 @@ namespace AlgorithmsDataStructures
 
     public T Get(string key)
     {
-      uint slot = HashFun(key);
-      if (slots[slot] == key) return slot;
+      uint slot = Convert.ToUInt32(HashFun(key));
+      if (slots[slot] == key) return values[slot];
       for (uint i = (slot + step) % size; i != slot; i = (i + step) % size)
-        if (slots[i] == key) return values(i);
+        if (slots[i] == key) return values[i];
       return default(T);
     }
   } 

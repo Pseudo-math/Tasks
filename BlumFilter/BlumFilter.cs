2d77702407896524b4637d662a3ee79d48aaa1c4
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-05-19 22:50:05 +0300

    Update BlumFilter.cs

--- BlumFilter/BlumFilter.cs
+++ BlumFilter/BlumFilter.cs
@@ -17,7 +17,7 @@ namespace AlgorithmsDataStructures
 
       public int Hash1(string str1)
       {
-        int code, randomNumber = 17;
+        int code = 0, randomNumber = 17;
         for(int i=0; i<str1.Length; i++)
         {
             code = (code * randomNumber + (int)str1[i]) % filter_len;
@@ -26,7 +26,7 @@ namespace AlgorithmsDataStructures
       }
       public int Hash2(string str1)
       {
-        int code, randomNumber = 223;
+        int code = 0, randomNumber = 223;
         for(int i=0; i<str1.Length; i++)
         {
             code = (code * randomNumber + (int)str1[i]) % filter_len;
@@ -36,15 +36,15 @@ namespace AlgorithmsDataStructures
 
       public void Add(string str1)
       {
-        filter = filter | (1 << Hash1(str1))  | (1 << Hash2(str1));
+        filter = filter | (uint)(1 << Hash1(str1))  | (uint)(1 << Hash2(str1));
       }
 
       public bool IsValue(string str1)
       {
-        if ((filter & (1 << Hash1(str1))) != 0 && (filter & (1 << Hash2(str1))) != 0) true;
+        if ((filter & (uint)(1 << Hash1(str1))) != 0 && (uint)(filter & (1 << Hash2(str1))) != 0) return true;
         return false;
       }
-   }
+    }
 }
 
 

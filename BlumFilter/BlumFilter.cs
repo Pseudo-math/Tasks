af1295a0d63095ad0388b31da0191981b1944e35
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-05-19 22:14:25 +0300

    Update BlumFilter.cs

--- BlumFilter/BlumFilter.cs
+++ BlumFilter/BlumFilter.cs
@@ -7,29 +7,31 @@ namespace AlgorithmsDataStructures
     public class BloomFilter
     {
       public int filter_len;
-
+      private BitArray BitArr;
+        
       public BloomFilter(int f_len)
       {
         filter_len = f_len;
-        // создаём битовый массив длиной f_len ...
+        BitArr  = new BitArray(f_len);
       }
 
-      // хэш-функции
       public int Hash1(string str1)
       {
-        // 17
+        int code, randomNumber = 17;
         for(int i=0; i<str1.Length; i++)
         {
-            int code = (int)str1[i];
+            code = (code * randomNumber + (int)str1[i]) % filter_len;
         }
-        // реализация ...
-        return 0;
+        return code;
       }
       public int Hash2(string str1)
       {
-        // 223
-        // реализация ...
-        return 0;
+        int code, randomNumber = 223;
+        for(int i=0; i<str1.Length; i++)
+        {
+            code = (code * randomNumber + (int)str1[i]) % filter_len;
+        }
+        return code;
       }
 
       public void Add(string str1)
